887e5413538937c7aaec01a5fd4220c76a6b6818
Rahul Deb <rahulbnc463@gmail.com>
2023-12-14 12:31:12 +0530

    start this project create folder structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# Get-Food-App
-This is a Food Application
---
+++ get-food/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="fs-1">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
