512b98ca30405b9fdef5f4a4d7078aeb466ed7a5
Rahul Deb <rahulbnc463@gmail.com>
2023-12-18 12:53:54 +0530

    create userSchema

--- backend/config/dbConnect.js
+++ backend/config/dbConnect.js
@@ -5,6 +5,14 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
     console.log("Connected to MongoDB");
+
+    //Getting Data from DB..............
+    const MainData = await mongoose.connection.db.collection("food_items");
+    const cursor = MainData.find({});
+    const dataArray = await cursor.toArray();
+
+    //log Data..............
+    // console.log(dataArray);
   } catch (error) {
     console.error("MongoDB connection error:", error);
   }
---
+++ backend/model/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose"); // Erase if already required
+const { schema } = mongoose;
+
+const userSchema = new schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+module.exports = mongoose.model("user", userSchema);
