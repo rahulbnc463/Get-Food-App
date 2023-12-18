deae6210b7b12a24bd7191f5016d55da18404d40
Rahul Deb <rahulbnc463@gmail.com>
2023-12-18 15:52:45 +0530

    start to create frontend part

--- backend/Routes/RegisterUser.js
+++ backend/Routes/RegisterUser.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const user = require("../model/userModel");
+const { body, validationResult } = require("express-validator");
 
-router.post("/register", async (req, res) => {
-  try {
-    await user.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-      location: req.body.location,
-    });
-    res.json({ success: true });
-  } catch (error) {
-    console.log("Error", error);
-    res.json({ success: false });
+router.post(
+  "/register",
+  [
+    body("email").isEmail(),
+    body("password", "Minimum 5 needed").isLength({ min: 8 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      await user.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location,
+      });
+      res.json({ success: true });
+    } catch (error) {
+      console.log("Error", error);
+      res.json({ success: false });
+    }
   }
-});
+);
 
 module.exports = router;
